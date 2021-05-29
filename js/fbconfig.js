0475a1db43ece1bdf995187b2124b73b937ac8d7
André Luferat <51307379+CataBits@users.noreply.github.com>
2021-05-29 12:44:04 -0300

    login/logout

---
+++ js/fbconfig.js
@@ -0,0 +1,13 @@
+// Configuração do Firebase
+// Altere conforme o SEU FIREBASE.com
+var firebaseConfig = {
+    apiKey: "AIzaSyAQOF8BDW1zD4bQ4Ahe-_BYWa2P3__szjA",
+    authDomain: "fuinhas-2acfa.firebaseapp.com",
+    projectId: "fuinhas-2acfa",
+    storageBucket: "fuinhas-2acfa.appspot.com",
+    messagingSenderId: "19868295442",
+    appId: "1:19868295442:web:496f3dc48740d2fa3b93cb"
+};
+
+// Initializa Firebase
+firebase.initializeApp(firebaseConfig);
--- js/global.js
+++ js/global.js
@@ -1,1 +1,79 @@
 // JavaScript do Aplicativo
+
+// Monitora usuário logado
+firebase.auth().onAuthStateChanged((user) => {
+
+    // Se tem alguém logado
+    if (user) {
+
+        // Monta 'view' do usuário logado
+        var html = `
+<div id="userLogged">        
+    <img src="${user.photoURL}" alt="${user.displayName}">
+    <div>${user.displayName}</div>
+</div>
+        `;
+
+        // Mostra usuário logado
+        $('#userData').html(html);
+
+        // Monitora click no usuário logado
+        $(document).on('click', '#userLogged', logout);
+    }
+    // Não tem ninguém logado
+    else {
+
+        var html = `
+<img src="/img/user.png" alt="Logue-se">
+<button id="btnLogin"><i class="fab fa-fw fa-google"></i> Entrar / Login</button>        
+        `;
+
+        // Mostra botão de login
+        $('#userData').html(html);
+
+        // Monitora botão "login"
+        $(document).on('click', '#btnLogin', login);
+    }
+});
+
+// Define o provedor de login social
+var provider = new firebase.auth.GoogleAuthProvider();
+
+// Força seleção da conta no login
+provider.setCustomParameters({ prompt: 'select_account' });
+
+// Login de usuário
+function login() {
+
+    // Define a persistência ao logar
+    // LOCAL, SESSION e NONE
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(
+
+            // Faz login usando janela pop-up
+            firebase.auth().signInWithPopup(provider)
+                .then()
+                .catch((error) => { console.error('Erro: ', error); })
+        )
+        .catch((error) => { console.error('Erro: ', error); });
+
+    return false;
+}
+
+// Logout de ususário
+function logout() {
+
+    // Confirmação
+    var msg = `Tem certeza que deseja sair do aplicativo?`;
+
+    if (confirm(msg)) {
+
+        // Faz logout do usuário, mantendo-o cadastrado 
+        firebase.auth().signOut();
+
+        // Remove o usuário do aplicativo e perde o Id
+        // firebase.auth().currentUser.delete();
+
+    }
+    return false;
+}
